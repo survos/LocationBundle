2d571b539f106f474356a7665ccdf68a2539758c
Tac Tacelosky <tacman@gmail.com>
2021-11-09 17:44:24 -0500

    Initial skeleton

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Survos\LocationBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree builder.
+     *
+     * @return TreeBuilder $builder The tree builder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $builder = new TreeBuilder('survos_location');
+
+        $rootNode = $builder->getRootNode();
+        $rootNode->children()
+            ->scalarNode('user_provider')
+                ->isRequired()
+                ->defaultValue('\App\Entity\User')
+            ->end()
+            ->arrayNode('bar')
+                ->isRequired()
+                ->scalarPrototype()
+                    ->defaultValue([
+                        'survos_location.ipsum',
+                        'survos_location.lorem',
+                    ])
+                ->end()
+            ->end()
+            ->integerNode('integer_foo')
+                ->isRequired()
+                ->defaultValue(2)
+                ->min(1)
+            ->end()
+            ->integerNode('integer_bar')
+                ->isRequired()
+                ->defaultValue(50)
+                ->min(1)
+            ->end()
+            ->end();
+
+        return $builder;
+    }
+}
---
+++ Entity/CarRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Survos\LocationBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class CarRepository extends EntityRepository
+{
+}

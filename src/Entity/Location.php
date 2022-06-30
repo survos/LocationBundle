aec7ddf02f8ab8f5fb479ad9b3a0e9d4e5887e75
Tac Tacelosky <tacman@gmail.com>
2022-06-30 17:08:32 -0400

    use attributes instead of annotations

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -2,33 +2,37 @@
 
 namespace Survos\LocationBundle\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Stringable;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Survos\Grid\Api\Filter\MultiFieldSearchFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 
-/**
- * @ORM\Entity(repositoryClass="Survos\LocationBundle\Repository\LocationRepository")
- * @ORM\Table(indexes={
- *     @ORM\Index(name="location_name_idx", columns={"name"}),
- *     @ORM\Index(name="location_lvl_idex", columns={"lvl"})
- * })
- * @Gedmo\Tree(type="nested")
- * @ApiResource()
- */
+#[ORM\Entity(repositoryClass: 'Survos\LocationBundle\Repository\LocationRepository')]
+#[ORM\Table(indexes: [new ORM\Index(name: 'location_name_idx', columns: ['name']), new ORM\Index(name: 'location_lvl_idex', columns: ['lvl'])])]
+#[Gedmo\Tree(type: 'nested')]
+#[ApiResource(
+    normalizationContext: ['skip_null_values' => false, 'groups' => ['rp', 'location.read', 'location.tree']],
+)]
+#[ApiFilter(OrderFilter::class, properties: ['code', 'name'], arguments: ['orderParameterName' => 'order'])]
+#[ApiFilter(MultiFieldSearchFilter::class, properties: ["code", 'name'], arguments: ["searchParameterName"=>"search"])]
+#[ApiFilter(SearchFilter::class, properties: ['name'=>'partial','code' => 'exact'])]
+
 class Location implements Stringable
 {
-
     public function __construct($code=null, $name=null, ?int $lvl=null)
     {
         $this->code = $code;
         $this->name = $name;
         $this->lvl = $lvl;
     }
-
     public static function build(string $code, string $name, ?int $lvl): self
     {
         $location = (new Location($code, $name, $lvl))
@@ -39,39 +43,27 @@ class Location implements Stringable
 
         return $location;
     }
-
-
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
-
-    /**
-     * @ORM\Column(type="string", length=180)
-     * @Assert\NotBlank()
-     */
+    #[ORM\Column(type: 'string', length: 180)]
+    #[Assert\NotBlank]
+    #[Groups(['location.read'])]
     private string $name;
-
-    /**
-     * @ORM\Column(type="string", length=180)
-     * @Assert\NotBlank()
-     */
+    #[ORM\Column(type: 'string', length: 180)]
+    #[Assert\NotBlank]
+    #[Groups(['location.read'])]
     private string $code;
-
-    /**
-     * @ORM\Column(type="integer", nullable=false)
-     */
+    #[ORM\Column(type: 'integer', nullable: false)]
+    #[Groups(['location.read'])]
     private int $lvl;
-
     /**
-     * @Gedmo\TreeLeft
-     * @ORM\Column(name="lft", type="integer")
      * @var int|mixed|null
      */
+    #[ORM\Column(name: 'lft', type: 'integer')]
+    #[Gedmo\TreeLeft]
     private $lft;
-
     /**
      * @return mixed|null
      */
@@ -79,7 +71,6 @@ class Location implements Stringable
     {
         return $this->lft;
     }
-
     /**
      * @param mixed $lft
      */
@@ -88,7 +79,6 @@ class Location implements Stringable
         $this->lft = $lft;
         return $this;
     }
-
     /**
      * @return mixed|null
      */
@@ -96,7 +86,6 @@ class Location implements Stringable
     {
         return $this->rgt;
     }
-
     /**
      * @param mixed $rgt
      */
@@ -105,7 +94,6 @@ class Location implements Stringable
         $this->rgt = $rgt;
         return $this;
     }
-
     /**
      * @return mixed|null
      */
@@ -113,7 +101,6 @@ class Location implements Stringable
     {
         return $this->root;
     }
-
     /**
      * @param mixed $root
      */
@@ -122,7 +109,6 @@ class Location implements Stringable
         $this->root = $root;
         return $this;
     }
-
     /**
      * @return mixed
      */
@@ -130,13 +116,11 @@ class Location implements Stringable
     {
         return $this->parent;
     }
-
     public function setParent(?Location $parent): self
     {
         $this->parent = $parent;
         return $this;
     }
-
     /**
      * @return mixed|null
      */
@@ -144,7 +128,6 @@ class Location implements Stringable
     {
         return $this->children;
     }
-
     /**
      * @param mixed $children
      */
@@ -153,7 +136,6 @@ class Location implements Stringable
         $this->children = $children;
         return $this;
     }
-
     /**
      * @return mixed
      */
@@ -161,89 +143,69 @@ class Location implements Stringable
     {
         return $this->lvl;
     }
-
     public function setLvl(?int $lvl): static
     {
         $this->lvl = $lvl;
         return $this;
     }
-
     /**
-     * @Gedmo\TreeRight
-     * @ORM\Column(name="rgt", type="integer")
      * @var int|mixed|null
      */
+    #[ORM\Column(name: 'rgt', type: 'integer')]
+    #[Gedmo\TreeRight]
     private $rgt;
-
     /**
-     * @Gedmo\TreeRoot
-     * @ORM\ManyToOne(targetEntity="Location", cascade={"persist"}, fetch="EAGER")
-     * @ORM\JoinColumn(name="tree_root", referencedColumnName="id", onDelete="CASCADE")
      * @var \Survos\LocationBundle\Entity\Location|mixed|null
      */
+    #[ORM\ManyToOne(targetEntity: 'Location', cascade: ['persist'], fetch: 'EAGER')]
+    #[ORM\JoinColumn(name: 'tree_root', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    #[Gedmo\TreeRoot]
     private $root;
-
-
-    /**
-     * @Gedmo\TreeParent
-     * @ORM\ManyToOne(targetEntity="Location", inversedBy="children", cascade={"persist"}, fetch="LAZY")
-     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", onDelete="CASCADE")
-     */
+    #[ORM\ManyToOne(targetEntity: 'Location', inversedBy: 'children', cascade: ['persist'], fetch: 'LAZY')]
+    #[ORM\JoinColumn(name: 'parent_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    #[Gedmo\TreeParent]
     private ?\Survos\LocationBundle\Entity\Location $parent = null;
-
-
     /**
-     * @ORM\OneToMany(targetEntity="Location", mappedBy="parent", cascade={"persist", "remove"}, fetch="LAZY")
      * @var \Survos\LocationBundle\Entity\Location[]|Collection|mixed|null
      */
+    #[ORM\OneToMany(targetEntity: 'Location', mappedBy: 'parent', cascade: ['persist', 'remove'], fetch: 'LAZY')]
     private ?Collection $children = null;
-
-    /**
-     * @ORM\Column(type="string", length=2, nullable=true)
-     */
+    #[ORM\Column(type: 'string', length: 2, nullable: true)]
     private ?string $alpha2 = null;
-
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getCode(): ?string
     {
         return $this->code;
     }
-
     public function setCode(string $code): self
     {
         $this->code = $code;
 
         return $this;
     }
-
     public function getName(): ?string
     {
         return $this->name;
     }
-
     public function setName(string $name): self
     {
         $this->name = $name;
 
         return $this;
     }
-
     public function getAlpha2(): ?string
     {
         return $this->alpha2;
     }
-
     public function setAlpha2(?string $alpha2): self
     {
         $this->alpha2 = $alpha2;
 
         return $this;
     }
-
     public function __toString(): string
     {
         return $this->getName();
